22d85a4460c4856eb890756eada236a8c07218b3
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-05-07 17:52:47 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,39 @@
+// 1- vazifa
+// let son1 = Number(prompt("Son1 ni kriting:"));
+// let son2 = Number(prompt("Son2 ni kriting:"));
+// let summa = son1 + son2;
+
+// alert(son1 + " + " + son2 + " = " + summa);
+
+
+// 2- vazifa
+//  let a = '12';
+//  let b = Number(a)
+
+// alert(b)
+
+
+// 3 -vazifa
+// let taxminiyson = Math.random() * 100
+// if (taxminiyson % 2 === 0) {
+
+//     console.log("taxminiyson juft")
+
+
+// } else {
+//     console.log( taxminiyson + " toq")
+// }
+
+
+// 4-vazifa 
+// 1-qadam: "soz" degan ozgaruvchiga
+// prompt orqali foydanuvchilardan malumot olib saqlab qoyamiz
+// let soz = prompt("");
+// 2-qadam: shu "soz" degan ozgaruvchi qiymatini teksirmiz
+// agar u "mars" ga teng bolsa alert ga "mars"  dagan sozni
+// aks holda "mars it school"  degan sozni chaqiramiz
+// if ( soz === "mars"){
+
+// }else{
+
+// }
